7d565e4f67eab4da5c5a513498a2d76c48a62427
danilozag1992 <danilozagarcanin@gmail.com>
2020-05-20 12:41:21 +0200

    changed options for confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: "https://gentle-dusk-43712.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://fathomless-anchorage-97470.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
